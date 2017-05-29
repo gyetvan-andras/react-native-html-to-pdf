229b7c537a3cb04fc7f1e68c6779e818d78cf0e5
Gyetván András <gyetvan.andras@me.com>
2017-05-29 13:18:51 +0200

    - add printhtml to Android version to avoid using non free itext package

--- android/src/main/java/com/parkerdan/htmltopdf/RNHTMLtoPDFModule.java
+++ android/src/main/java/com/parkerdan/htmltopdf/RNHTMLtoPDFModule.java
@@ -10,12 +10,16 @@ import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReadableType;
 import com.facebook.react.bridge.WritableMap;
 
+import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.UiThreadUtil;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
 import java.io.InputStream;
 import java.io.ByteArrayInputStream;
 import java.util.UUID;
+
 
 import com.itextpdf.text.Document;
 import com.itextpdf.text.Paragraph;
@@ -26,12 +30,29 @@ import com.itextpdf.text.Rectangle;
 
 import android.os.Environment;
 import android.util.Log;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.CancellationSignal;
+import android.os.ParcelFileDescriptor;
+import android.print.pdf.PrintedPdfDocument;
+import android.print.PageRange;
+import android.print.PrintAttributes;
+import android.print.PrintDocumentAdapter;
+import android.print.PrintDocumentInfo;
+import android.print.PrintManager;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class RNHTMLtoPDFModule extends ReactContextBaseJavaModule {
 
   private Promise promise;
   private final ReactApplicationContext mReactContext;
-
+	WebView mWebView;
   public RNHTMLtoPDFModule(ReactApplicationContext reactContext) {
     super(reactContext);
     mReactContext = reactContext;
@@ -129,5 +150,69 @@ public class RNHTMLtoPDFModule extends ReactContextBaseJavaModule {
       throw new Exception(e);
     }
   }
-	
+
+  @ReactMethod
+  public void printhtml(final String html, final Promise promise) {
+
+    final String jobName = "Document";
+
+    try {
+      UiThreadUtil.runOnUiThread(new Runnable() {
+          @Override
+          public void run() {
+              // Create a WebView object specifically for printing
+              WebView webView = new WebView(mReactContext);
+              webView.setWebViewClient(new WebViewClient() {
+                public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                    return false;
+                }
+
+                @Override
+                public void onPageFinished(WebView view, String url) {
+                    // Get the print manager.
+                    PrintManager printManager = (PrintManager) getCurrentActivity().getSystemService(
+                            Context.PRINT_SERVICE);
+                    // Create a wrapper PrintDocumentAdapter to clean up when done.
+                    PrintDocumentAdapter adapter = new PrintDocumentAdapter() {
+                        private final PrintDocumentAdapter mWrappedInstance =
+                                mWebView.createPrintDocumentAdapter();
+                        @Override
+                        public void onStart() {
+                            mWrappedInstance.onStart();
+                        }
+                        @Override
+                        public void onLayout(PrintAttributes oldAttributes, PrintAttributes newAttributes,
+                                CancellationSignal cancellationSignal, LayoutResultCallback callback,
+                                Bundle extras) {
+                            mWrappedInstance.onLayout(oldAttributes, newAttributes, cancellationSignal,
+                                    callback, extras);
+                        }
+                        @Override
+                        public void onWrite(PageRange[] pages, ParcelFileDescriptor destination,
+                                CancellationSignal cancellationSignal, WriteResultCallback callback) {
+                            mWrappedInstance.onWrite(pages, destination, cancellationSignal, callback);
+                        }
+                        @Override
+                        public void onFinish() {
+                            mWrappedInstance.onFinish();
+                        }
+                    };
+                    // Pass in the ViewView's document adapter.
+                    printManager.print(jobName, adapter, null);
+                    mWebView = null;
+                    promise.resolve(jobName);
+                }
+              });
+
+              webView.loadDataWithBaseURL(null, html, "text/HTML", "UTF-8", null);
+
+              // Keep a reference to WebView object until you pass the PrintDocumentAdapter
+              // to the PrintManager
+              mWebView = webView;
+          }
+      });
+    } catch (Exception e) {
+      promise.reject("print_error", e);
+    }
+  }	
 }
